48aeef02433d5f628386d9d2cbe682de25344b47
Esra Kastrati <esra.kastrati6@gmail.com>
2020-12-08 00:16:14 -0500

    fix navbar for aschool.component

--- code/src/components/aschools.js
+++ code/src/components/aschools.js
@@ -215,42 +215,12 @@ export default class aschools extends Component {
                             <div class="sb-sidenav-menu">
                                 <div class="nav">
                                     <div class="sb-sidenav-menu-heading">Core</div>
-                                    <a class="nav-link" href="/new">
+                                    <a class="nav-link" href="/secretary">
                                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                        Add new Account
+                                        Dashboard
                             </a>
-                                    <div class="sb-sidenav-menu-heading">Interface</div>
-                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                        Students Profile
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                                            <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                                        </nav>
-                                    </div>
-                                    <a class="nav-link collapsed" href="/newClassroom" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                        Create Classroom
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                        Display Associated Schools
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                        Manage Student Profile
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                        Manage Teacher Profile
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
+                                   
+                            
 
                                     <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
                                         <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
